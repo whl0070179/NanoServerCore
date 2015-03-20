14a4d38ff1a5fb1b7942a0cd2dafca6d8b19faf8
whl0070179 <whl0070179@163.com>
2015-03-21 00:18:29 +0800

    [王宏亮]Nano服务器引擎

---
+++ NanoServerCore/RPC/HttpRPCAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NanoServerCore
+{
+    /// <summary>
+    /// RPC 调用类型
+    /// </summary>
+    public enum RPCType
+    {
+        Admin,  // 
+        Player,
+        Server,
+    }
+
+    [System.AttributeUsage(System.AttributeTargets.Method)]
+    public class HttpRPC
+    {
+    }
+}
